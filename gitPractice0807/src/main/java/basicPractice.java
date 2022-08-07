d63b8ddb11ecc112d042802da71e5a9522c2d081
huang123ying <741962719@qq.com>
2022-08-07 14:48:24 +0800

    基本操作的练习

---
+++ gitPractice0807/src/main/java/basicPractice.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: huangying
+ * @date: 2022-08-07 14:46
+ */
+public class basicPractice {
+    public static void main(String[] args) {
+        System.out.println("这是一个git基本操作的练习");
+    }
+}
